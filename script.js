5e7366038a8eac148b292aa21f7c241f9d771415
AmalAlshehrig <amalalshehrig@gmail.com>
2023-11-07 07:26:29 +0300

    add

---
+++ script.js
@@ -0,0 +1,32 @@
+let BoxCat=document.getElementById("boxcat")
+let h1banner=document.getElementById("h1banner")
+let img1=document.getElementById("img1")
+let h31=document.getElementById("h31")
+let h32=document.getElementById("h32")
+let p1=document.getElementById("p1")
+let cboxh1=document.getElementById("cboxh1")
+let cboxp1=document.getElementById("cboxp1")
+let cbox3=document.getElementById("cbox3")
+let cboxh11=document.getElementById("cboxh11")
+let cboxp11=document.getElementById("cboxp11")
+let cbox6=document.getElementById("cbox6")
+let cboxh111=document.getElementById("cboxh111")
+let cboxp111=document.getElementById("cboxp111")
+let cbox8=document.getElementById("cbox8")
+BoxCat.addEventListener("click",function fun(){
+    h1banner.innerText="We're the Cat People Book trusted, local pet sitters and walkers who will care for your cat like you would."
+    img1.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRauJ6evjDqOODiZ0VkSH0AGIh3ko7BfCTB2g&usqp=CAU";
+    h31.innerText="Cat Boarding"
+    h32.innerText="Cat walking"
+    p1.innerText="Whenever your cat needs a walk."
+    cboxh1.innerText="Siamese"
+    cboxp1.innerText="Coming from the ancient kingdom of Siam (now Thailand), it was then brought to England and the United States at the end of the 19th century"
+    cbox3.src="https://7esl.com/wp-content/uploads/2021/06/Siamese.png"
+    cboxh11.innerText="Snowshoe"
+    cboxp11.innerText="A breed native to the United States, the Snowshoe is descended from the Siamese, the two-tone American Shorthair, and the Sacred of Burma."
+    cbox6.src="https://7esl.com/wp-content/uploads/2021/06/Snowshoe.png"
+    cboxh111.innerText="Sokoke"
+    cboxp111.innerText="Initially, it was called Khadzonzos (rind) to recall the particular motif of the geometric designs of the mantle that recall not only the bark of the tree but also the marble."
+    cbox8.src="https://7esl.com/wp-content/uploads/2021/06/Sokoke.png"
+
+})
